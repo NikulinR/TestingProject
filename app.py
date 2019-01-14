38e3080283dfcd4f404235275bf3b0aa3d1edae5
NikulinR <RomanNikulin1@yandex.ru>
2019-01-14 23:08:06 +0300

    creating simple text question

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 from flask import Flask, render_template,g, request, redirect, url_for
 
 DATABASE = 'Testing.db'
@@ -7,8 +8,11 @@ db = sqlite3.connect(DATABASE, check_same_thread=False)
 cur = db.cursor()
 isLogined = False
 isFirstTry = True
+Collision = False
 testName = ''
 user = ''
+curQID = 0
+
 
 class DBItem():
     
@@ -22,15 +26,23 @@ class DBItem():
         cur.execute("delete from %s where %s = '%s';"%(self.tablename, attribute, value))
         db.commit()
         
-    def Create(self, args):
+    def Create(self, args, withID):
         cur.execute('select * from '+self.tablename)
-        desc = str(list(map(lambda x: x[0], cur.description))).strip('[').strip(']').rstrip(',')
         
-        print("insert into %s (%s) values (%s);"%(self.tablename,desc,str(args).strip('(').strip(')').rstrip(',')))
+    
         
-        cur.execute("insert into %s (%s) values (%s);"%(self.tablename,desc,str(args).strip('(').strip(')').rstrip(',')))
-        #cur.execute("insert or replace into %s values %s"%(self.tablename,args))  
-        db.commit()
+        try:
+            if withID:
+                print("insert into %s (%s) values (%s);"%(self.tablename,str(list(map(lambda x: x[0], cur.description))).strip('[').strip(']').rstrip(','),str(args).strip('(').strip(')').rstrip(',')))
+                cur.execute("insert into %s (%s) values (%s);"%(self.tablename,str(list(map(lambda x: x[0], cur.description))).strip('[').strip(']').rstrip(','),str(args).strip('(').strip(')').rstrip(',')))
+            else:
+                print("insert into %s (%s) values (%s);"%(self.tablename,str(list(map(lambda x: x[0], cur.description))[1:]).strip('[').strip(']').rstrip(','),str(args).strip('(').strip(')').rstrip(',')))
+                cur.execute("insert into %s (%s) values (%s);"%(self.tablename,str(list(map(lambda x: x[0], cur.description))[1:]).strip('[').strip(']').rstrip(','),str(args).strip('(').strip(')').rstrip(',')))
+            db.commit()
+        
+        except:
+            global Collision
+            Collision=True
         
     def Edit(self, setid, newrow):
         cur.execute('select * from '+self.tablename)
@@ -46,7 +58,7 @@ class DBItem():
         cur.execute(requestline)
     
     def Show(self):
-        print('select * from '+self.tablename+";")
+        #print('select * from '+self.tablename+";")
         cur.execute('select * from '+self.tablename+";")
         return cur.fetchall()
     
@@ -65,7 +77,8 @@ def main():
             isFirstTry = False
             return render_template("signin.html", repass = False)
     else:
-        return render_template("index.html", user = user[0][0], group = user[0][2].lower())
+        tests = DBItem('Test').Show()
+        return render_template("index.html", user = user[0][0], group = user[0][2].lower(), tests=tests)
 
 @app.route("/signin", methods=['POST'])
 def signin():
@@ -87,9 +100,16 @@ def signout():
 @app.route("/create", methods=['POST'])
 def create():
     global testName
+    global Collision
     testName = request.form['testname']
     if not testName:
         return main()
+    DBItem('Test').Create((testName, int(time.time())),True)
+    if Collision:
+        Collision = False
+        return main()
+    global curQID
+    curQID=0
     return render_template("create.html", testname=testName)
 
 @app.route("/edit", methods=['POST'])
@@ -98,9 +118,33 @@ def edit():
 
 @app.route("/delete", methods=['POST'])
 def delete():
+    DBItem('Test').Delete('Name', request.form['testname'])
+    return main()
+
+@app.route("/nextq", methods=['POST'])
+def nextq():
+    qtype = request.form['type']
+    question = request.form['qname']
+    testname = request.form['testname']
+    global curQID
+    curQID+=1
+    DBItem('Question').Create((str(testname)+'.'+str(curQID),testname,question,qtype),True)
+    if qtype=='isText':
+        qu = 'you chosed '+request.form['textanswer']+' in '+qtype+question
+        print(qu)
+        DBItem('Answer').Create((request.form['textanswer'],1,str(testname)+'.'+str(curQID)),False)
+    if qtype=='isRadio':
+        qu = 'you chosed '+request.form['corradioanswer']+' in '+qtype+question
+        print(qu)
+        
+    if qtype=='isCheck':
+        qu = 'you chosed '+str(request.form.getlist('corcheckanswer'))+' in '+qtype+question
+        print(qu) 
+    return render_template("create.html", testname=testname)
+
+@app.route("/finishq", methods=['POST'])
+def finishq():
     return "delete"
 
-
-
 if __name__ == '__main__':
-  app.run(debug = True)
+  app.run(debug = False)
